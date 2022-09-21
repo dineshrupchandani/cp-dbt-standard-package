334ad31d4fb5e2cdcc7ed7c07c26e1b709d806f1
mishraranjay <bp_ranjay_mishra@colpal.com>
2022-09-21 12:47:27 -0400

    new version 2.0.0

---
+++ models/pr_metrics_view.sql
@@ -0,0 +1,25 @@
+{{ config(materialized='view') }}
+select
+    github_context:job as job_name,
+    github_context:actor as triggered_by,
+    github_context:base_ref as base_branch,
+    github_context:head_ref as featured_branch,
+    github_context:event.enterprise.created_at as created_time,
+    github_context:event.enterprise.updated_at as updated_time,
+    github_context:repository as repository_name,
+    github_context:run_id as run_id,
+    github_context:event_name as event_name,
+    github_context:event.action as event_action_type,
+    github_context:event.pull_request.state as state,
+   -- '{message}',
+    github_context:event.number as event_number,
+    github_context:event.pull_request.assignee as Reviewed_by,
+    WORKFLOW_START_TIME,
+    WORKFLOW_END_TIME,
+    concat('[', REGEXP_REPLACE(newly_added_models, '\n', ','), ']') newly_added_models,
+    concat('[', REGEXP_REPLACE(modified_models, '\n', ','), ']') modified_models,
+    concat('[', REGEXP_REPLACE(dependent_modified_models, '\n', ','), ']') dependent_modified_models,
+    concat('[', REGEXP_REPLACE(deleted_models, '\n', ','), ']') deleted_models,
+    EVENT_NAME,
+    JOB_STATUS
+from "DEV_DBT_TESTING_RAW"."UTIL_COMMON"."PR_METRICS_RAW"
