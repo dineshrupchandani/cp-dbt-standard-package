15d161303cc814d848436716c13a7c1aabc34727
mishraranjay <bp_ranjay_mishra@colpal.com>
2022-09-22 00:18:38 -0400

    create view - dbt metrics

--- models/pr_metrics_view.sql
+++ models/pr_metrics_view.sql
@@ -1,11 +1,13 @@
 {%- set source_relation = adapter.get_relation(
-      database="{{ env_var('ARTIFACTS_DATABASE')}}",
-      schema="{{ env_var('ARTIFACTS_SCHEMA')}}",
+      database={{ env_var('ARTIFACTS_DATABASE') }},
+      schema={{ env_var('ARTIFACTS_SCHEMA') }},
       identifier="pr_metrics_raw") -%}
+
+{{ log("Source Relation: " ~ source_relation, info=true) }}
 
 {% set table_exists=source_relation is not none   %}
 
-{{ config(materialized='view', tags='pr_metrics') }}
+{{ config(materialized='view'}}
 
 {% if table_exists %}
 
