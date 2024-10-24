668a198a4ede410f4fa85279647df9fdabef7c34
bp-jeremy-nguyen-cp <bp_jeremy_nguyen@colpal.com>
2024-10-24 14:04:26 -0400

    Update materialization_iceberg_table.sql

--- macros/materializations/materialization_iceberg_table.sql
+++ macros/materializations/materialization_iceberg_table.sql
@@ -163,3 +163,54 @@
         "relations": [this]
     }) }}
 {% endmaterialization %}
+
+{% macro data_type_mapping(data_type) %}
+{{ log("DATA TYPE LENGTH = " ~ data_type, info=True) }}
+    {% set mapped_data_type = data_type | lower %}
+    {% if 'character varying' in mapped_data_type or 'varchar' in mapped_data_type %}
+            VARCHAR
+    {% else %}
+        {{ data_type }}
+    {% endif %}
+{% endmacro %}
+
+{% macro iceberg_insert_columns(temp_view_name) %}
+{% set model_columns = [] %}
+{# set model_columns = adapter.get_columns_in_relation(this) #}
+ {% set desc_temp_view_sql %}
+        DESCRIBE VIEW {{ this.database}}.{{ this.schema }}.{{ temp_view_name }} 
+    {% endset %} 
+    {% set model_columns = run_query(desc_temp_view_sql) %}
+{% set comma = joiner(", ") %}
+{% for column in model_columns %}
+    {{ comma() }}{{ column.name }}
+    {#{ log("model cols from insert cols macro: " ~ current_columns | map(attribute='name') | join(', '), info=True)}#}
+{% endfor %}
+{% endmacro %}
+
+{% macro iceberg_insert_values(source, temp_view_name) %}
+{% set model_columns = [] %}
+{# set model_columns = adapter.get_columns_in_relation(this) #}
+ {% set desc_temp_view_sql %}
+        DESCRIBE VIEW {{ this.database}}.{{ this.schema }}.{{ temp_view_name }} 
+    {% endset %} 
+    {% set model_columns = run_query(desc_temp_view_sql) %}
+{% set comma = joiner(", ") %}
+{% for column in model_columns %}
+    {{ comma() }}{{ source }}.{{ column.name }}
+{% endfor %}
+{% endmacro %}
+
+{% macro iceberg_update_columns(source, target, temp_view_name) %}
+{#{ log("SOURCE SQL = " ~ source, info=True)}#}
+{% set model_columns = [] %}
+{#  set model_columns = adapter.get_columns_in_relation(this)  #}
+    {% set desc_temp_view_sql %}
+        DESCRIBE VIEW {{ this.database}}.{{ this.schema }}.{{ temp_view_name }} 
+    {% endset %} 
+    {% set model_columns = run_query(desc_temp_view_sql) %}
+{% set comma = joiner(", ") %}
+{% for column in model_columns %}
+    {{ comma() }}{{ target }}.{{ column.name }} = {{ source }}.{{ column.name }}
+{% endfor %}
+{% endmacro %}
