190ddf7824368435767150e29a65f244cb5bb415
bp-jeremy-nguyen-cp <bp_jeremy_nguyen@colpal.com>
2024-10-24 14:06:24 -0400

    Update materialization_iceberg_table.sql

--- macros/materializations/materialization_iceberg_table.sql
+++ macros/materializations/materialization_iceberg_table.sql
@@ -1,3 +1,17 @@
+{% macro iceberg_update_columns(source, target, temp_view_name) %}
+{#{ log("SOURCE SQL = " ~ source, info=True)}#}
+{% set model_columns = [] %}
+{#  set model_columns = adapter.get_columns_in_relation(this)  #}
+    {% set desc_temp_view_sql %}
+        DESCRIBE VIEW {{ this.database}}.{{ this.schema }}.{{ temp_view_name }} 
+    {% endset %} 
+    {% set model_columns = run_query(desc_temp_view_sql) %}
+{% set comma = joiner(", ") %}
+{% for column in model_columns %}
+    {{ comma() }}{{ target }}.{{ column.name }} = {{ source }}.{{ column.name }}
+{% endfor %}
+{% endmacro %}
+    
 {% materialization  iceberg_table, default %}
 {# Getting settings from model #}
     {% set catalog = model.config.get('catalog', 'SNOWFLAKE') %}
@@ -198,19 +212,5 @@
 {% set comma = joiner(", ") %}
 {% for column in model_columns %}
     {{ comma() }}{{ source }}.{{ column.name }}
-{% endfor %}
-{% endmacro %}
-
-{% macro iceberg_update_columns(source, target, temp_view_name) %}
-{#{ log("SOURCE SQL = " ~ source, info=True)}#}
-{% set model_columns = [] %}
-{#  set model_columns = adapter.get_columns_in_relation(this)  #}
-    {% set desc_temp_view_sql %}
-        DESCRIBE VIEW {{ this.database}}.{{ this.schema }}.{{ temp_view_name }} 
-    {% endset %} 
-    {% set model_columns = run_query(desc_temp_view_sql) %}
-{% set comma = joiner(", ") %}
-{% for column in model_columns %}
-    {{ comma() }}{{ target }}.{{ column.name }} = {{ source }}.{{ column.name }}
 {% endfor %}
 {% endmacro %}
